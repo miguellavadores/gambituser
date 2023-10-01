f9f4ff4ecc21658afd8d2166ee1df1e5554f2abe
Miguel Angel Lavadores <miguel.lavadores@grupofiglobal.com>
2023-10-01 13:55:51 -0600

    Ultimo Commit

---
+++ bd/signup.go
@@ -0,0 +1,30 @@
+package bd
+
+import (
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/miguellavadores/gambituser/models"
+	"github.com/miguellavadores/gambituser/tools"
+)
+
+func SignUp(sig models.SignUp) error {
+	fmt.Println("Comienza Registro")
+	err := DbConnect()
+	if err != nil {
+		return err
+	}
+	defer Db.Close()
+
+	sentencia := "INSERT INTO users (User_Email, User_UUID, User_DateAdd) VALUES ('" + sig.UserEmail + "','" + sig.UserUUID + "','" + tools.FechaMySQL() + "')"
+	fmt.Println(sentencia)
+
+	_, err = Db.Exec(sentencia)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	fmt.Println("SignUp > Ejecución Exitosa")
+	return nil
+}
